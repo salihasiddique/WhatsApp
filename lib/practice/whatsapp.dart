d1ca5962956cbcb9924e5753a532376bc3fd3e26
salihasiddique <salihasiddik1999@gmail.com>
2024-06-13 10:12:16 +0530

    completed

---
+++ lib/practice/whatsapp.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: whatsapp(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+class whatsapp extends StatefulWidget {
+  const whatsapp({super.key});
+
+  @override
+  State<whatsapp> createState() => _whatsappState();
+}
+
+class _whatsappState extends State<whatsapp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Text("Contacts",style: GoogleFonts.aBeeZee(
+
+          ),),
+          Card(
+            child: ListTile(
+              title: Text("saliha"),
+              subtitle: Text("good morning"),
+              trailing: Icon(Icons.notifications),
+              leading: Image.asset("assets/images/lion.png"),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text("home"),
+              subtitle: Text("will call you later"),
+              trailing: Icon(Icons.notifications),
+              leading: Image.asset("assets/images/house.png"),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text("amazon"),
+              subtitle: Text("track your order"),
+              trailing: Icon(Icons.notifications),
+              leading: Image.asset("assets/images/cart.png"),
+            ),
+          )
+        ],
+      ),
+    );
+
+  }
+}
